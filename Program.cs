ac0229b225ba495bedc3681cb0e4613f33cfddc2
UGRCAN <ugur.can@massiveprocess.com>
2021-12-15 16:38:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var subscriber = new SubscriberSocket())
+            {
+                subscriber.Connect("tcp://publisher:1234"); //publisher is container name of publisher( due to ip binding )
+                subscriber.Subscribe("A"); //A is topic name
+
+                while (true)
+                {
+                    //var topic = subscriber.ReceiveFrameString();
+                    var topicAndMsg = subscriber.ReceiveFrameString();
+                    Console.WriteLine("From Server:  {0}", topicAndMsg.Substring(1));
+                }
+            }
+        }
+    }
+}
